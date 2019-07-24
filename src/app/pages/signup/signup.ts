2c930d6e98445812b70f2863be5e6472310ddcb7
abelsco <abelsmenezes@gmail.com>
2019-07-23 23:37:52 -0300

    Arrumando

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -3,7 +3,6 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { UserData } from '../../providers/user-data';
-import { DatabaseService, Cliente } from '../../providers/database.service';
 
 import { UserOptions } from '../../interfaces/user-options';
 
@@ -17,22 +16,18 @@ import { UserOptions } from '../../interfaces/user-options';
 export class SignupPage {
   signup: UserOptions = { username: '', password: '', endSilo: '', tipoGrao: '' };
   submitted = false;
-  public cliente: Cliente
 
   constructor(
     public router: Router,
-    public userData: UserData,
-    public dbs: DatabaseService,
+    public userData: UserData
   ) {}
 
   onSignup(form: NgForm) {
     this.submitted = true;
 
     if (form.valid) {
-      // this.userData.signup(this.signup.username, this.signup.tipoGrao);
+      this.userData.signup(this.signup.username, this.signup.tipoGrao);
       // Aqui a definição dos parámetros
-      this.dbs.addCliente(this.signup.username, this.signup.password, this.signup.username, this.signup.endSilo, this.signup.tipoGrao);
-      this.cliente = <Cliente><any>this.dbs.loginCliente(this.signup.username, this.signup.password);
       this.router.navigateByUrl('/app/tabs/schedule');
     }
   }
