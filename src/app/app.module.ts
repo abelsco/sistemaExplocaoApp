23cfd87b8ce96a453da5dd03f485ef5a995e7172
abelsco <abelsmenezes@gmail.com>
2019-08-30 01:20:29 -0300

    Adicionando LOCALE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +16,7 @@ import { environment } from '../environments/environment';
 import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
 import { SQLite } from '@ionic-native/sqlite/ngx';
 
+registerLocaleData(localePt);
 @NgModule({
   imports: [
     BrowserModule,
@@ -30,6 +33,7 @@ import { SQLite } from '@ionic-native/sqlite/ngx';
   ],
   declarations: [AppComponent],
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     InAppBrowser,
     SplashScreen,
     StatusBar,
