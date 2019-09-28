4c49c6dc2e7d80713f86c5949220227a47c64c88
abelsco <abelsmenezes@gmail.com>
2019-09-28 02:01:17 -0300

    Arrumando data.service

--- src/app/providers/db-data.service.ts
+++ src/app/providers/db-data.service.ts
@@ -1,10 +1,14 @@
 import { Cliente, Silo, Leitura } from './../interfaces/user-options';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DbDataService {
   // private hostStorage: string = '192.168.16.254';
   private hostStorage: string = '192.168.16.4';
@@ -15,31 +19,68 @@ export class DbDataService {
     private httpClient: HttpClient,
   ) {  }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
   postCliente(dados: any) {
-    return this.httpClient.post<Silo>(this.url_storage + 'cliente/', dados);
+    return this.httpClient.post<Silo>(this.url_storage + 'cliente/', dados).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   postSenhaCliente(dados: any) {
-    return this.httpClient.post<any>(this.url_storage + 'cliente/senha/', dados);
+    return this.httpClient.post<any>(this.url_storage + 'cliente/senha/', dados).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   postNomeSilo(dados: any) {
-    return this.httpClient.post<any>(this.url_storage + 'cliente/nomesilo/', dados);
+    return this.httpClient.post<any>(this.url_storage + 'cliente/nomesilo/', dados).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   postTipoGrao(dados: any) {
-    return this.httpClient.post<any>(this.url_storage + 'cliente/tipograo/', dados);
+    return this.httpClient.post<any>(this.url_storage + 'cliente/tipograo/', dados).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   postDeletaCliente(dados: any) {
-    return this.httpClient.post<any>(this.url_storage + 'cliente/deleta/', dados);
+    return this.httpClient.post<any>(this.url_storage + 'cliente/deleta/', dados).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
-  
-  getLogin(usuario: string, senha: string){
-    return this.httpClient.get<Silo>(this.url_storage + 'login?usuario=' + usuario + '&senha=' + senha);    
+
+  getLogin(usuario: string, senha: string) {
+    return this.httpClient.get<Silo>(this.url_storage + 'login?usuario=' + usuario + '&senha=' + senha).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   getAmbi(atual: Silo) {
-    return this.httpClient.get<Leitura>(this.url_storage + 'ambiente?codSilo=' + atual.codSilo);
+    return this.httpClient.get<any>(this.url_storage + 'ambiente?codSilo=' + atual.codSilo).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 }
